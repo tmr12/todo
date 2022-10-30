b05b01b37c4e986136c0698e543195a84bb383de
Madhava <madhavarao.tnr@gmail.com>
2022-10-30 13:55:05 +0530

    PA9:until chpter-2__twoway data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todoItem';
+import { TodoList } from './todoList';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList("TMR", [new TodoItem("Go fo run", true),
+  new TodoItem("Get flowers"),
+  new TodoItem("Collect Tickets")]);
+
+  get userName(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items;
+  }
+
+  
 }
